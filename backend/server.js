468d9b5340ef9c8df1229742dd7627e93ccae550
AaryaShewale07 <aaryashewale0307@gmail.com>
2026-07-06 22:54:39 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -49,8 +49,9 @@ app.use(
 
 // ============ CORS CONFIGURATION ============
 const allowedOrigins = [
-  'http://localhost:5173',
+  'http://localhost:3000',
   'https://rawcanvasstudio1.netlify.app',
+
 ]
 
 app.use(
